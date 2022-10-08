ccc57fea5b1c958e850b0e1d69ba61abf9a34cbe
Grzegorz Krol <gk@cjk.pl>
2022-10-09 00:05:09 +0200

    Added CircleCI config and a test project

---
+++ testproject/testproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-mn%m-&gx)j@$rn74m64o(jc=81#4$ju=4#bo9+$&k6a5rx3mp!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

4342218cba9d31a4b668fc3fa49a697a9f309c65
Grzegorz Krol <gk@cjk.pl>
2022-11-30 10:59:17 +0100

    Removed trailing whitespace

--- cjkcms/templatetags/auth_extras.py
+++ cjkcms/templatetags/auth_extras.py
@@ -1,10 +1,10 @@
 from django import template
-from django.contrib.auth.models import Group 
+from django.contrib.auth.models import Group
 
 register = template.Library()
 
 
-@register.filter(name='has_group')
+@register.filter(name="has_group")
 def has_group(user, group_name):
     """Filter to check if current user belongs to specific group"""
     group = Group.objects.get(name=group_name)

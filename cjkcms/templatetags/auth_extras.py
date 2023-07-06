49c3f5578d8e9e962cd04219636d9d085c40841e
Grzegorz Krol <gk@cjk.pl>
2023-07-06 22:57:03 +0200

    Added  tag used in multiple apps dependent on cjkcms. @Todo: remove this tag from these apps, e.g. users, etc

--- cjkcms/templatetags/auth_extras.py
+++ cjkcms/templatetags/auth_extras.py
@@ -9,3 +9,8 @@ def has_group(user, group_name):
     """Filter to check if current user belongs to specific group"""
     group = Group.objects.get(name=group_name)
     return group in user.groups.all()
+
+
+@register.simple_tag
+def define(val=None):
+    return val

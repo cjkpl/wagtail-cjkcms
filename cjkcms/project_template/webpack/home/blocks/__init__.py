cfbcc0ecbe8d18602ae23e3307065ba819b3fac6
Grzegorz Krol <gk@cjk.pl>
2025-01-31 23:38:28 +0100

    Restructured block imports layout in new project templates

--- cjkcms/project_template/webpack/home/blocks/__init__.py
+++ cjkcms/project_template/webpack/home/blocks/__init__.py
@@ -1,2 +1,14 @@
 from .blocks import EventPresentationBlock  # noqa
-from .blocks import PROJECT_CONTENT_STREAMBLOCKS, PROJECT_LAYOUT_STREAMBLOCKS  # noqa
+from .blocks import HOME_CONTENT_STREAMBLOCKS, HOME_LAYOUT_STREAMBLOCKS  # noqa
+#from otherapp.blocks import OTHERAPP_CONTENT_STREAMBLOCKS, OTHERAPP_LAYOUT_STREAMBLOCKS  # noqa
+
+
+# add your own content block
+PROJECT_CONTENT_STREAMBLOCKS = HOME_CONTENT_STREAMBLOCKS + [
+    ("event_presentation", EventPresentationBlock()),
+]
+
+# add your own layout blocks
+PROJECT_LAYOUT_STREAMBLOCKS = HOME_LAYOUT_STREAMBLOCKS + [
+    # ("custom_block", CustomBlock()),
+]
--- cjkcms/project_template/webpack/home/blocks/blocks.py
+++ cjkcms/project_template/webpack/home/blocks/blocks.py
@@ -66,11 +66,11 @@ class EventPresentationBlock(BaseBlock):
 
 
 # add your own content block
-PROJECT_CONTENT_STREAMBLOCKS = [
+HOME_CONTENT_STREAMBLOCKS = [
     ("event_presentation", EventPresentationBlock()),
 ]
 
 # add your own layout blocks
-PROJECT_LAYOUT_STREAMBLOCKS = [
+HOME_LAYOUT_STREAMBLOCKS = [
     # ("custom_block", CustomBlock()),
 ]

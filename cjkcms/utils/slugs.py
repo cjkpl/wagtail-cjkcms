7fdf809baba220371b23b43955b025d8a0ffdbf4
Grzegorz Krol <gk@cjk.pl>
2025-02-06 18:21:00 +0100

    Removed unused file

--- cjkcms/utils/slugs.py
+++
@@ -1,9 +0,0 @@
-from django.utils.text import slugify as django_slugify
-from unidecode import unidecode
-
-
-def custom_slugify(value):
-    # Convert to ASCII characters
-    ascii_value = unidecode(value)
-    # Use Django's built-in slugify after converting to ASCII
-    return django_slugify(ascii_value)

91676ce3e3b7675297db39a44223a6fd85d5e810
Grzegorz Krol <gk@cjk.pl>
2025-02-07 22:51:27 +0100

    Added tests and (and missing implementation) to ensure custom_id and custom_css is used in all blocks for cards, cardgrids, pagelist, quote

--- cjkcms/tests/test_settings.py
+++ cjkcms/tests/test_settings.py
@@ -25,34 +25,45 @@ class SettingsTests(TestCase):
             # self.assertIsNone(abs_path, f"{logo} not found.")
             self.assertTrue(str(abs_path).endswith(logo), f"{logo} not found.")
 
-    def _check_templates(self, templates, skip_empty=False):
+    def _check_templates(self, templates, skip_empty=False, check_advanced=False):
         """Check if all declared template files specified in the `templates` list
         are accessible and execute without error. Used to check individual
-        sections in settings.py"""
+        sections in settings.py. Skip empty templates if `skip_empty` is True.
+        Check advanced settings features optionally (custom_id, custom_css_class)"""
 
         for tpl in templates:
             if skip_empty and not tpl[0]:
                 continue
             try:
-                get_template(tpl[0])
+                tpl = get_template(tpl[0])
+                # if check_advanced, ensure that strings {{self.settings.custom_css_class}}
+                # and {{self.settings.custom_id}} are present in template source
+                if check_advanced:
+                    self.assertIn("{{self.settings.custom_css_class}}", tpl.template.source)
+                    self.assertIn("{{self.settings.custom_id}}", tpl.template.source)
+
             except TemplateDoesNotExist as e:
                 self.fail(f"Template not found: {str(e)}")
 
     def test_cardblock_templates(self):
         templates = cms_settings.CJKCMS_FRONTEND_TEMPLATES_BLOCKS["cardblock"]
-        self._check_templates(templates)
+        self._check_templates(templates, check_advanced=True)
 
     def test_cardgridblock_templates(self):
         templates = cms_settings.CJKCMS_FRONTEND_TEMPLATES_BLOCKS["cardgridblock"]
-        self._check_templates(templates)
+        self._check_templates(templates, check_advanced=True)
 
     def test_pagelistblock_templates(self):
         templates = cms_settings.CJKCMS_FRONTEND_TEMPLATES_BLOCKS["pagelistblock"]
-        self._check_templates(templates)
+        self._check_templates(templates, check_advanced=True)
 
     def test_pagepreviewblock_templates(self):
         templates = cms_settings.CJKCMS_FRONTEND_TEMPLATES_BLOCKS["pagepreviewblock"]
         self._check_templates(templates)
+
+    def test_quoteblock_templates(self):
+        templates = cms_settings.CJKCMS_FRONTEND_TEMPLATES_BLOCKS["quoteblock"]
+        self._check_templates(templates, check_advanced=True)
 
     def test_frontend_template_pages_templates(self):
         templates = cms_settings.CJKCMS_FRONTEND_TEMPLATES_PAGES["*"]
